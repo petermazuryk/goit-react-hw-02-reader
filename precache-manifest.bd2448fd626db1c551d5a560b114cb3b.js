c9a8fd8e1f1e793620aa79c84ee7adb61015f336
Peter Mazuryk <petermazuryk1984@gmail.com>
2020-01-10 20:56:17 +0200

    Updates

---
+++ precache-manifest.bd2448fd626db1c551d5a560b114cb3b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1705bf043cd3bd911050ee7e9b4d234",
+    "url": "/goit-react-hw-02-reader/index.html"
+  },
+  {
+    "revision": "f8ba4c3ebc98cbd60fb9",
+    "url": "/goit-react-hw-02-reader/static/css/main.0593113b.chunk.css"
+  },
+  {
+    "revision": "336dc4b791b0ed370dd1",
+    "url": "/goit-react-hw-02-reader/static/js/2.878f6841.chunk.js"
+  },
+  {
+    "revision": "f8ba4c3ebc98cbd60fb9",
+    "url": "/goit-react-hw-02-reader/static/js/main.5212a45c.chunk.js"
+  },
+  {
+    "revision": "7a8d446f429a5ab8ff75",
+    "url": "/goit-react-hw-02-reader/static/js/runtime-main.f3c961e0.js"
+  }
+]);
